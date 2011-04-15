8670ca1874cc54f303d5608ed11cbd70798cea45
HIROE Shin <hiroe.orz@gmail.com>
2011-04-15 11:58:20 +0900

    initial commit

--- spec/css_handler_spec.rb
+++ spec/css_handler_spec.rb
@@ -1,7 +1,147 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "CssHandler" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "CssHandler", :with => :new do
+  before :all do
+    @css_1 =<<EOF
+body {
+  overflow: hidden;
+  font-family: Arial, Verdana, sans-serif;
+  font-size: 12px;
+  background-color: #333;
+}
+
+/* use iframe layout */
+.iframe-body {
+  overflow: hidden;
+  font-family: Arial, Verdana, sans-serif;
+  font-size: 12px;
+  background-color: #111;
+}
+/* use iframe-black layout */
+.iframe-body-black {
+  overflow: hidden;
+  font-family: Arial, Verdana, sans-serif;
+  font-size: 15px;
+  background-color: #333;
+}
+
+.not-overrided-class {
+  background-color: #fff;
+}
+EOF
+
+    @css_2 =<<EOF
+body {
+  overflow: hidden;
+  font-family: Arial, Verdana, sans-serif;
+  font-size: 12px;
+  background-color: #333;
+}
+
+/* use iframe layout */
+.iframe-body {
+  overflow: hidden;
+  font-family: Arial, Verdana, sans-serif;
+  font-size: 12px;
+  background-color: #222;
+}
+/* use iframe-black layout */
+.iframe-body-black {
+  overflow: hidden;
+  font-family: Arial, Verdana, sans-serif;
+  font-size: 13px;
+  background-color: #333;
+}
+
+.overrided-class {
+  background-color: #000;
+}
+EOF
+    make_css(@css_1, @css_file_path_1)
+    make_css(@css_2, @css_file_path_2)
   end
+
+  before :each do
+    @handler_1 = CssHandler.new(@css_file_path_1)
+    @handler_2 = CssHandler.new(@css_file_path_2)
+  end
+
+  it "should create instance with filepath" do
+    @handler_1.class.should == CssHandler
+    @handler_2.class.should == CssHandler
+  end
+
+  it "should call method using element name" do
+    obj = @handler_1.objects[".iframe-body-black"]
+    obj.class.should == CssElement
+    obj.overflow.should == "hidden"
+    obj.font_family.should == ["Arial", "Verdana", "sans-serif"]
+    obj.font_size.should == "15px"
+    obj.background_color.should == "#333"
+
+    obj2 = @handler_2.objects[".iframe-body-black"]
+    obj2.font_size.should == "13px"
+  end
+
+  it "should create same css" do
+    css_body = @handler_1.css_same_entry_string(@handler_2)
+    css_body.should_not =~ /font-size: 13px/
+    css_body.should_not =~ /font-size: 15px/
+    css_body.should_not =~ /background-color: #111/
+    css_body.should_not =~ /background-color: #222/
+
+    css_body.should ==
+"body {
+  overflow: hidden;
+  font-family: Arial, Verdana, sans-serif;
+  font-size: 12px;
+  background-color: #333;
+}
+
+.iframe-body {
+  overflow: hidden;
+  font-family: Arial, Verdana, sans-serif;
+  font-size: 12px;
+}
+
+.iframe-body-black {
+  overflow: hidden;
+  font-family: Arial, Verdana, sans-serif;
+  background-color: #333;
+}
+"
+  end
+
+  it "should create css diff" do
+    css_body = @handler_1.override_entry_string(@handler_2)
+    css_body.should ==
+".overrided-class {
+  background-color: #000;
+}
+
+.iframe-body {
+  background-color: #222;
+}
+
+.iframe-body-black {
+  font-size: 13px;
+}
+"
+
+    css_body = @handler_2.override_entry_string(@handler_1)
+    css_body.should ==
+".not-overrided-class {
+  background-color: #fff;
+}
+
+.iframe-body {
+  background-color: #111;
+}
+
+.iframe-body-black {
+  font-size: 15px;
+}
+"
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,22 @@ require 'css_handler'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
+  config.before(:all) do
+    @base_dir = File.expand_path("..", File.dirname(__FILE__))
+    @tmp_dir = File.expand_path("tmp", @base_dir)
+    @css_file_path_1 = File.expand_path("test_1.css",  @tmp_dir)
+    @css_file_path_2 = File.expand_path("test_2.css",  @tmp_dir)
+    
+    Dir.mkdir(@tmp_dir) unless File.exist?(@tmp_dir)
+  end
+
+  config.after(:all) do
+    File.unlink(@css_file_path_1) if File.exist?(@css_file_path_1)
+    File.unlink(@css_file_path_2) if File.exist?(@css_file_path_2)
+    Dir.unlink(@tmp_dir) if File.exist?(@tmp_dir)
+  end
+end
+
+def make_css(str, path)
+  File.open(path, "w") { |f| f.write(str) }
 end
